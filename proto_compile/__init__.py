9e5f8f0d4df511f6c7eadcb79d6f3aaaae85c139
romnnn <roman.dahm@gmx.de>
2021-09-11 21:34:16 +0200

    expose core functions and classes

--- proto_compile/__init__.py
+++ proto_compile/__init__.py
@@ -5,3 +5,17 @@
 __author__ = """romnn"""
 __email__ = "contact@romnn.com"
 __version__ = "0.1.3"
+
+from proto_compile.options import BaseCompilerOptions, CompilerOptions, CompileTarget
+from proto_compile.proto_compile import compile, compile_grpc_web, compile_python_grpc
+from proto_compile.versions import Target
+
+__all__ = [
+    "compile",
+    "compile_grpc_web",
+    "compile_python_grpc",
+    "Target",
+    "BaseCompilerOptions",
+    "CompileTarget",
+    "CompilerOptions",
+]
